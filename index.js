aaf9437e4a996ab71516be5e843a51d9952cc04d
Tahsin Reza <syedtahsinreza74789@gmail.com>
2026-05-26 21:54:10 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello');
+console.log('I like Javascript');
+
+window.alert('Warning!');
+window.alert('BUUUUUUU!');
+
+document.getElementById("myH1").textContent= "Hello";
+document.getElementById("myP").textContent= "This p text is written by javascript";
+
+// this is a comment
+
+/* this is a multi line 
+comment */
+
